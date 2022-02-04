c72aa330f40b94e0cefc0fab6214599a2d158d3c
Samuel Ocran <sammiestt@gmail.com>
2022-02-04 18:41:10 +0100

    Deploy with Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,16 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="ocranbillions/mlapi"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run --image=ocranbillions/mlapi mlapi --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pod
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/mlapi 8000:80
